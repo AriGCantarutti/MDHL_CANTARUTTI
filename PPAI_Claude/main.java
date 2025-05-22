3da84d60eecd2d55902122f26325e0d72f53bc04
Ariana Cantarutti <101531976+AriGCantarutti@users.noreply.github.com>
2025-05-22 20:14:16 -0300

    Delete PPAI_Claude directory

--- PPAI_Claude/main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main (String[] args) {
-        GestorOrden gestor = new GestorOrden(0, "empleadoRI", 1, "datosOC", "motivo", "comentario", "confirmacion", 1, "emailResponsable", "notificacionesEmail", "monitor");
-        gestor.opcCerrarOrden();
-    }
-}
